3140e1391a758d1e7d6fb5d8baad306f15878bda
kimminna <kmina121777@naver.com>
2025-09-15 00:11:51 +0900

    feat: API 응답값 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,35 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allBooks, randomBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]); // 병렬 호출
+  return {
+    props: { allBooks, randomBooks },
+  };
+};
+export default function Home({
+  allBooks,
+  randomBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {randomBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,25 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
-export default function Page() {
+// context : 브라우저로부터 받은 매개변수의 모든 정보가 들어 있다
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+  return { props: { books } };
+};
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
-      {books.map((book) => {
-        <BookItem key={book.id} {...book} />;
-      })}
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 }
