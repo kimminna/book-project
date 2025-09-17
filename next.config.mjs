b58d45c7cb11b436007652544313cc1a22703779
kimminna <kmina121777@naver.com>
2025-09-17 20:42:36 +0900

    fix: 외부 이미지 도메인 경로 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net",
+      },
+    ],
+  },
 };
-
 export default nextConfig;
